88695d6b61a46d8aad2b35b86819929720e0aa67
Sumukh Atreya <sumukhatreya@Sumukhs-MacBook-Pro.local>
2021-03-14 23:31:16 +0530

    Added project dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+
 class App extends React.Component {
+
   render() {
     return(
-      <div>Hello World!</div>
+      <div></div>
     )
   }
 }
