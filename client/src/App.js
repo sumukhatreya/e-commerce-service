f52a9bb433109f134d00605e55fcd0ec77dc9124
Sumukh Atreya <sumukhatreya@Sumukhs-MacBook-Pro.local>
2021-03-14 17:58:33 +0530

    Basic project setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>Hello World!</div>
+    )
+  }
+}
+
+export default App;
