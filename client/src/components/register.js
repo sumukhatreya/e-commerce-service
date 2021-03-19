5de2136fa3188566676728da5a50e6f5f882dafa
Sumukh Atreya <sumukhatreya@Sumukhs-MacBook-Pro.local>
2021-03-19 18:29:49 +0530

    Updated error handling for user registration.

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const validateUsername = async (user) => {
     const username = user.trim();
     const validate = true;
-    const url = 'http://localhost:5000/register/'
+    const url = 'http://localhost:5000/registers/'
     try {
         const res = await fetch(url, {
             method: 'POST',
@@ -13,25 +13,48 @@ const validateUsername = async (user) => {
             headers: {'Content-Type': 'application/json'}
         });
         console.log(res);
-        if (res.status === 202) {
-            return true;
-        } else {
-            return false;
+        if (!res.ok) {
+            console.log(typeof res.status);
+            if (res.status <= 451) {
+                const jsonRes = await res.json();
+                console.log(jsonRes);
+                throw new Error(jsonRes.message);
+            } else if (res.status >= 500) {
+                console.log('Status text', res.statusText);
+                throw new Error(res.statusText);
+            }
         }
+        return { validated: true, error: null };
     } catch (err) {
-        console.log(err);
-        // return false;
+        console.log('This is the error', err);
+        return { validated: false, error: err.message };
     }
 }
 
+const fetchData = async (requestType, url, payload, headers) => {
+    try {
+        const res = fetch(url, {
+            method: requestType,
+            body: payload,
+            headers: headers
+        });
+        return res;
+    } catch (err) {
+
+    }
+    
+
+}
+
 export default function RegistrationForm() {
+    const [error, setError] = useState(null);
 
     const formik = useFormik({
         initialValues: {
             firstName:'',
             lastName:'',
             email:'',
-            // dateOfBirth:'',
+            dateOfBirth:'',
             username:'',
             password:'',
             repeatedPassword:''
@@ -40,7 +63,7 @@ export default function RegistrationForm() {
             firstName: Yup.string().trim().required('Required'),
             lastName: Yup.string().trim().required('Required'),
             email: Yup.string().trim().email('Please enter a valid email').required('Required'),
-            // dateOfBirth: Yup.date().required('Required'),
+            dateOfBirth: Yup.date().required('Required'),
             username: Yup.string().trim().required('Required'),
             password: Yup.string().min(7, 'Your password should be at least 7 charcaters long')
                 .required('Required'),
@@ -48,22 +71,25 @@ export default function RegistrationForm() {
                 .required('Required')
         }),
         onSubmit: async (values) => {
+            console.log(values, values.username);
+            console.log(formik.errors);
             try {
                 console.log(values, values.username);
-                const validated = await validateUsername(values.username);
-                console.log('Validated', validated);
+                const { validated, error } = await validateUsername(values.username);
+                console.log('Validated', validated, 'Error', error);
                 if (validated) {
+                    setError(null);
                     console.log('fetch api call here');
-                } else if (!validated) {
-                    throw new Error('Username already in use');
                 } else {
-                    throw new Error('Some other error');
-                }
+                    throw new Error(error);
+                }   
             } catch (err) {
-                console.log(err);
+                console.log(err.message);
+                setError(err.message);
             }
         }
     });
+    // console.log(formik.values);
 
     return(
         <div className='registration-form'>
@@ -75,12 +101,16 @@ export default function RegistrationForm() {
                        value={formik.values.firstName}
                        onChange={formik.handleChange}/>
 
-                <label htmlFor='firstName'>Last name</label>
+                {formik.touched && formik.errors.firstName && <h2>{formik.errors.firstName}</h2>}
+
+                <label htmlFor='lastName'>Last name</label>
                 <input type='text' 
                        id='lastName' 
                        name='lastName' 
                        value={formik.values.lastName}
                        onChange={formik.handleChange}/>
+
+                {formik.touched && formik.errors.lastName && <h2>{formik.errors.lastName}</h2>}
 
                 <label htmlFor='email'>Email</label>
                 <input type='email' 
@@ -89,12 +119,16 @@ export default function RegistrationForm() {
                        value={formik.values.email}
                        onChange={formik.handleChange}/>
 
-                {/* <label htmlFor='dateOfBirth'>Date of birth</label>
+                {formik.touched && formik.errors.email && <h2>{formik.errors.email}</h2>}
+
+                <label htmlFor='dateOfBirth'>Date of birth</label>
                 <input type='date' 
                        id='dateOfBirth' 
-                    //    name='email' 
+                       name='dateOfBirth' 
                        value={formik.values.dateOfBirth}
-                       onChange={formik.handleChange}/> */}
+                       onChange={formik.handleChange}/>
+
+                {formik.touched && formik.errors.dateOfBirth && <h2>{formik.errors.dateOfBirth}</h2>}
 
                 <label htmlFor='username'>Username</label>
                 <input type='string' 
@@ -103,6 +137,8 @@ export default function RegistrationForm() {
                        value={formik.values.username}
                        onChange={formik.handleChange}/>
 
+                {formik.touched && formik.errors.username && <h2>{formik.errors.username}</h2>}
+
                 <label htmlFor='password'>Password</label>
                 <input type='password' 
                        id='password' 
@@ -110,15 +146,20 @@ export default function RegistrationForm() {
                        value={formik.values.password}
                        onChange={formik.handleChange}/>
 
+                {formik.touched && formik.errors.password && <h2>{formik.errors.password}</h2>}
+
                 <label htmlFor='repeatedPassword'>Confirm password</label>
                 <input type='password' 
                        id='repeatedPassword' 
                        name='repeatedPassword' 
                        value={formik.values.repeatedPassword}
                        onChange={formik.handleChange}/>
+                
+                {formik.touched && formik.errors.repeatedPassword && <h2>{formik.errors.repeatedPassword}</h2>}
 
                 <button type='submit'>Register</button>
             </form>
+            {error && <h2>{error}</h2>}
         </div>
     )
 }
