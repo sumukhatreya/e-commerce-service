9e8bb055b096e54e5788210a5c78644db27382b8
Sumukh Atreya <sumukhatreya@Sumukhs-MacBook-Pro.local>
2021-06-12 23:51:43 +0530

    Added backend functionality for rating and review CRUD operations

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { findOne } = require('../models/product');
 const ProductEntry = require('../models/product');
 const RatingsAndReviews = require('../models/productRatingsAndReviews');
 const { verifyJWT } = require('../utils');
@@ -43,23 +44,27 @@ router.post('/:id', async (req, res, next) => {
 router.get('/:id/review', async (req, res, next) => {
     try {
         // const header = verifyJWT(req);
-        const header = 'quack';
+        const header = 'ada';
         const headers = { 'Access-Control-Expose-Headers': 'isLoggedIn', 'isLoggedIn': header };
         res.set(headers);
         if (header === '') {
             res.status(401);
             throw new Error('Unauthorized user');
         }
-
-        // The following code is very inefficient; I'm loading the entire document from memory and then searching for the relevant review entry, instead of directly loading the particular review entry object from the database in a single atomic operation (I couldn't figure out how to do this - see comment below).
-        const ratingAndReviewEntry = await RatingsAndReviews.findOne({ productRef: req.params.id }, 'ratingsAndReviews');
+        // The following code is inefficient: I'm loading the entire document from memory and then searching for the relevant review entry, instead of directly loading the relevant review object from the database in a single atomic operation (I couldn't figure out how to do this - see comment below).
+        let userReview = null;
+        const ratingAndReviewEntry = await RatingsAndReviews.findOne({ productRef: req.params.id, 'ratingsAndReviews.username': header }, 'ratingsAndReviews');
         console.log('ratingAndReviewEntry', ratingAndReviewEntry);
-        const userReview = ratingAndReviewEntry.ratingsAndReviews.find(entry => entry.username === header);
-
-        // The below line of code works, but it returns the entire document, not only the relevant subdocument, if there's a match. This seems to be the case with MongoDB. Every tutorial I've looked at for subdocument retrieval from an array says the same thing. I suppose I'll have to iterate through the entire document at the application level to find the relevant subdocument.
+        if (!ratingAndReviewEntry) {
+            res.status(204);
+        } else {
+            userReview = ratingAndReviewEntry.ratingsAndReviews.find(entry => entry.username === header);
+            res.status(200);
+        }
+        // The below (commented) line of code works, but it returns the entire document, not only the relevant subdocument, if there's a match. This seems to be the case with MongoDB. Every tutorial I've looked at for subdocument retrieval from an array says the same thing. I suppose I'll have to iterate through the entire document at the application level to find the relevant subdocument.
         // const userReview = await RatingsAndReviews.findOne({ productRef: req.params.id, ratingsAndReviews: { $elemMatch: { 'username': 'groucho' } } });
         console.log('userReview', userReview);
-        res.status(200).json(userReview);
+        res.json(userReview);
     } catch (err) {
         next(err);
     }
@@ -68,7 +73,7 @@ router.get('/:id/review', async (req, res, next) => {
 router.post('/:id/review', async (req, res, next) => {
     try {
         // const header = verifyJWT(req);
-        const header = 'grouchomarx2';
+        const header = 'alan';
         const headers = { 'Access-Control-Expose-Headers': 'isLoggedIn', 'isLoggedIn': header };
         res.set(headers);
         if (header === '') {
@@ -93,27 +98,31 @@ router.post('/:id/review', async (req, res, next) => {
     }
 });
 
-// Unhappy with this code. I had wanted to use Mongoose queries to retrieve and update subdocuments within the ratingsAndReviews array, but I couldn't figure out how. I'll revisit this code later.
 router.put('/:id/review', async (req, res, next) => {
     try {
-        const header = verifyJWT(req);
+        // const header = verifyJWT(req);
+        const header = 'ada';
         const headers = { 'Access-Control-Expose-Headers': 'isLoggedIn', 'isLoggedIn': header };
         res.set(headers);
         if (header === '') {
             res.status(401);
             throw new Error('Unauthorized user');
-        }
-        const ratingAndReviewEntry = await RatingsAndReviews.find({ productRef: req.params.id });
-        // ratingAndReviewEntry.
-        // console.log(ratingAndReviewEntry);
-        const ratingArray = ratingAndReviewEntry[0].ratingsAndReviews;
-        let desiredEntry = ratingArray.find(entry => entry.username === header);
-        // console.log(desiredEntry);
-        desiredEntry.rating = req.body.rating;
-        desiredEntry.review = req.body.review;
-        desiredEntry.lastUpdated = Date.now();
-        // console.log(desiredEntry);
-        res.status(200).json({ message: 'Rating and review entry modified' });
+        } 
+        const netUserRating = req.body.oldRating - req.body.rating;
+        const ratingAndReviewEntry = await RatingsAndReviews.findOneAndUpdate({ productRef: req.params.id, 'ratingsAndReviews.username': header }, 
+        { $set: 
+            { 'ratingsAndReviews.$.review': req.body.review, 
+              'ratingsAndReviews.$.rating': req.body.rating, 
+              'ratingsAndReviews.$.lastUpdated': Date.now() 
+            },
+          $inc: { ratingsAggregate: netUserRating }
+        }, 
+        { new: true });
+        console.log(ratingAndReviewEntry);
+        const updatedRating = (ratingAndReviewEntry.ratingsAggregate / ratingAndReviewEntry.numOfRatings).toFixed(1);
+        const productEntry = await ProductEntry.findByIdAndUpdate({ _id: req.params.id }, { rating: updatedRating}, { new: true });
+        console.log(productEntry);
+        res.status(200).json(ratingAndReviewEntry);
     } catch (err) {
         next(err);
     }
@@ -121,15 +130,23 @@ router.put('/:id/review', async (req, res, next) => {
 
 router.delete('/:id/review', async (req, res, next) => {
     try {
-        // const header = 'ssa';
-        // const result = await RatingsAndReviews.findByIdAndUpdate(req.params.id, {
-        //     $pull: {
-        //         ratingsAndReviews: {
-        //             'username': header
-        //         }
-        //     }
-        // });
-        // res.status(200).json(result);
+        // const header = verifyJWT(req);
+        const header = 'ssa';
+        const headers = { 'Access-Control-Expose-Headers': 'isLoggedIn', 'isLoggedIn': header };
+        res.set(headers);
+        if (header === '') {
+            res.status(401);
+            throw new Error('Unauthorized user');
+        }
+        const ratingAndReviewEntry = await RatingsAndReviews.findOneAndUpdate({ productRef: req.params.id, 'ratingsAndReviews.username': header }, { $pull: { ratingsAndReviews: { username: header }}, $inc: { ratingsAggregate: -req.body.rating, numOfRatings: -1 }}, { new: true });
+
+        const updatedRating = (ratingAndReviewEntry.ratingsAggregate / ratingAndReviewEntry.numOfRatings).toFixed(1);
+
+        const productEntry = await ProductEntry.findByIdAndUpdate({ _id: req.params.id }, { rating: updatedRating}, { new: true });
+
+        console.log(productEntry);
+
+        res.status(200).json(ratingAndReviewEntry);
     } catch (err) {
         next(err);
     }
